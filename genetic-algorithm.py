97696fff41b15cc725efa892c23da614211cbf24
tirelessR <ipacucka@gmail.com>
2026-09-22 16:39:57 +0300

    added def with indiv creation

--- genetic-algorithm.py
+++ genetic-algorithm.py
@@ -1,3 +1,20 @@
 from math import sin, cos
+import random
 
-func = lambda x, y: sin(x) * cos(y) + x
+# Определение максимальных констант
+MAX_X = 5
+MAX_Y = 5
+
+# Определение минимальных констант
+MIN_X = -5
+MIN_Y = -5
+
+# Определение функции
+def func(x, y):
+    return sin(x) * cos(y) + x
+
+# Создаём случайную особь
+def create_individual():
+    individual = [random.uniform(MIN_X, MAX_X),
+                  random.uniform(MIN_Y, MAX_Y)]
+    return individual
