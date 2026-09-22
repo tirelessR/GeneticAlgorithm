967459a7e6c5ea38da09450c4de08171f45fa26e
tirelessR <ipacucka@gmail.com>
2026-09-22 19:19:10 +0300

    added mutation and inversion

--- genetic-algorithm.py
+++ genetic-algorithm.py
@@ -9,6 +9,9 @@ MAX_Y = 5
 MIN_X = -5
 MIN_Y = -5
 
+# Шаг инверсии
+EPS = 0.2
+
 # Определение функции
 def func(x, y):
     return sin(x) * cos(y) + x
@@ -19,8 +22,24 @@ def create_individual():
                   random.uniform(MIN_Y, MAX_Y)]
     return individual
 
+# Кроссовер
 def crossover(parent1, parent2):
     child = [(parent1[0] + parent2[0]) / 2,
              (parent1[1] + parent2[1]) / 2]
     return child
 
+# Мутация
+def mutation():
+    return create_individual()
+
+# Инверсия
+def inversion(ind, EPS):
+    sign_x = random.choice([-1, 1])
+    new_x = ind[0] + sign_x * EPS
+    new_x = max(MIN_X, min(MAX_X, new_x))
+
+    sign_y = random.choice([-1, 1])
+    new_y = ind[1] + sign_y * EPS
+    new_y = max(MIN_Y, min(MAX_Y, new_y))
+
+    return [new_x, new_y]
